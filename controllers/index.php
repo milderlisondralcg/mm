979dec587882a0e825413f40821e3e128addb2f7
Milder Lisondra <milder.lisondra@yahoo.com>
2018-12-04 15:18:22 -0800

    Add files via upload

--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ALL);
 spl_autoload_register('mmAutoloader');
 
 function mmAutoloader($className){
@@ -10,45 +10,71 @@ function mmAutoloader($className){
 
 $media = new Media();
 
+/***************** Load Azure classes **************************/
+
+require_once '../../azureblob/vendor/autoload.php';
+//require_once "./random_string.php";
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+//$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=mmblobcohr;AccountKey=4aOouaoDgYheE+hJNhUQL9FEOlr/Cqc2qhqJ0RV0DKqudxfyzvzm8v2l3ojjnwPWLSIx5xNUSP5M5B0uBIxtEg==";
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+/***************************************************************/
+
+
 // Uploads location
 $uploads_path = "../uploads/";
 
 
-if($_FILES){
-	$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt','tiff'); // valid extensions
+if($_FILES){ print_r($_FILES);
+	$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt','tiff','zip','csv','xls','xlsx','sql','txt'); // valid extensions
 	
 	//$path = '../uploads/'; // upload directory
 
-	if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['image']){
-		$img = $_FILES['image']['name'];
-		$tmp = $_FILES['image']['tmp_name'];
+	if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['file_upload']){
+		$uploaded_filename = $_FILES['file_upload']['name'];
+		$tmp = $_FILES['file_upload']['tmp_name'];
 
 		// get uploaded file's extension
-		$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+		$ext = strtolower(pathinfo($uploaded_filename, PATHINFO_EXTENSION));
 
 		// can upload same image using rand function
-		$final_image = rand(1000,1000000).$img;
+		//$final_image = rand(1000,1000000)."-".$img;
 
 		// check's valid format
 		if(in_array($ext, $valid_extensions)) { 
-			$final_image = strtolower($final_image); 
-			$final_path = $uploads_path.$final_image; 
+			//$final_image = strtolower($final_image); 
+			//$final_path = $uploads_path.$final_image; 
+			$moved_filename = trim(strtolower($uploaded_filename));
+			$final_path = $uploads_path.$moved_filename; 
 
 			if(move_uploaded_file($tmp,$final_path)) {
 				$file_mime_type = mime_content_type($final_path);
-			//echo "<img src='$path' />";
-				$_POST['saved_media'] = $final_image;
-				$data = array("user"=>"milder.lisondra@yahoo.com","action"=>"Add new media","object"=>"Media","previous_data"=>"N/A","updated_data"=>$file_mime_type);
-				$media->add($_POST);
-				$media->log_action($data);
+				$_POST['saved_media'] = $moved_filename;
+				$result = $media->add($_POST);
+				
+				$log_data = array("user"=>"milder.lisondra@yahoo.com","action"=>"Add new media","object"=>"Media","previous_data"=>"N/A","updated_data"=>$file_mime_type);
+				$media->log_action($log_data); // Log admin action
+				
+				if( $result['result'] == true){
+					$azure_filename = strtolower(str_replace(" " ,"-",$_POST['Title'])) . "-" . $result['ID']. "." . $ext;
+					// Need to update the record with the filename actually stored in Azure
+					$update_data = array("saved_media"=>$azure_filename,"id"=>$result['ID']);
+					$media->update($update_data);
+					$containerName = "files";
+					$content = fopen($final_path, "r");
+					//Upload media asset to Azure Blob Storage
+					$azure_upload_result = $blobClient->createBlockBlob($containerName, $azure_filename, $content);	
+
+				}
 			}
 		}else{
 			echo 'invalid';
 		}
 	}
-}
-
-// log actions taken by admins
-function log_action(){
-	
 }
