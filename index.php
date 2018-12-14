f29ad1dd5b59e79a0d68a75d0eb5b7b42d4bf132
Milder Lisondra <milder.lisondra@yahoo.com>
2018-12-13 18:31:24 -0800

    Redirects to home.php

--- index.php
+++ index.php
@@ -1,159 +1,29 @@
 <?php
-include('includes/header.php');
-?>
-<div class="main-wrapper">
-    <div class="contents">
-        <div class="heading">
-            <h2>Media List</h2>
-        </div>
+header("Location: /_media_manager/home.php");
+//check for parameter
 
-        <div class="page-contents">
-            <div>
-                <div class="group">
-                    <table id="documents">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Category</th>
-                                <th>Description</th>
-                                <th>Salesforce ID</th>
-                                <th>Alternate Tracking ID</th>
-                                <th>Lead Form Required</th>
-                                <th>Email to Notify</th>
-                                <th>Date Added</th>
-                                <th>Last Modified</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Title</th>
-                                <th>Category</th>
-                                <th>Description</th>
-                                <th>Salesforce ID</th>
-                                <th>Alternate Tracking ID</th>
-                                <th>Lead Form Required</th>
-                                <th>Email to Notify</th>
-                                <th>Date Added</th>
-                                <th>Last Modified</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-<?php /*
-                            @foreach (var doc in ViewBag.DocsList)
-                            {
-                                if (doc.Tags == "" || doc.Tags == null)
-                                {
-                                    doc.Tags = "N/A";
-                                }
-                                else
-                                {
-                                    doc.Tags.Trim();
-                                }
-                                if (doc.SalesForceId == "" || doc.SalesForceId == null)
-                                {
-                                    doc.SalesForceId = "N/A";
-                                }
-                                else
-                                {
-                                    doc.SalesForceId.Trim();
-                                }
-                                if (doc.AlternateTrackingId == "" || doc.AlternateTrackingId == null)
-                                {
-                                    doc.AlternateTrackingId = "N/A";
-                                }
+/*
+if( !isset($_GET['user']) ){
+	header("Location: /_media_manager/noaccess.php");
+}else{
 
-                                if (doc.Category == "file")
-                                {
-                                    doc.Category = "File";
-                                }
-                                if (doc.Category == "emailblasts")
-                                {
-                                    doc.Category = "Email Blasts";
-                                }
-                                if (doc.Category == "assets")
-                                {
-                                    doc.Category = "Assets";
-                                }
+$url = 'https://charlie.coherent.com/api/authenticate/?userid=' . $_GET['user'];
+$curl_handler = curl_init();
+curl_setopt($curl_handler, CURLOPT_URL, $url);
+//curl_setopt($curl_handler, CURLOPT_HTTPGET, true);
+curl_setopt($curl_handler, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Accept: application/json'
+));
+
+$result = curl_exec($curl_handler);
+curl_close($curl_handler);
+//print_r($result);
+
+//print json_decode($result);
+
+	// TODO: Session variable
+	//header("Location: /_media_manager/home.php");
+}
+
 */
-?>
-                                <tr>
-                                    <td><a href="/Document/DocumentDetails/@doc.Id">Title</a></td>
-                                    <td>Category</td>
-                                    <td>Description</td>
-									<td>SalesForceId</td>
-                                    <td>AlternateTrackingId</td>
-                                    <td>LeadRequired</td>
-                                    <td>EmailNotify</td>
-                                    <td>CreatedDateTime</td>
-                                    <td>ModifiedDateTime</td>
-                                </tr>
-                        </tbody>
-
-                    </table>
-                </div>
-            </div>
-    </div>
-</div>
-
-
-<script>
-var data = [
-    {
-        "Title":       "Beam Master",
-        "Category":   "M-LMC",
-        "Description":     "",
-		"Salesforce ID":"", 
-		"Alternate Tracking ID":"",
-		"Lead Form Required":"Yes",
-		"Email to Notify": "web@coherent.com",
-		"Date Added":"",
-		"Last Modified":""		
-    },
-    {
-        "Title":       "Beam Viewer",
-        "Category":   "M-LMC",
-        "Description":     "",
-		"Salesforce ID":"", 
-		"Alternate Tracking ID":"",
-		"Lead Form Required":"Yes",
-		"Email to Notify": "web@coherent.com",
-		"Date Added":"",
-		"Last Modified":""		
-		
-    },
-    {
-        "Title":       "EngergyMax PC v1.2.0.2",
-        "Category":   "M-LMC",
-        "Description":     "",
-		"Salesforce ID":"", 
-		"Alternate Tracking ID":"",
-		"Lead Form Required":"N/A",
-		"Email to Notify": "",
-		"Date Added":"",
-		"Last Modified":""		
-		
-    }	
-];
-    $(document).ready(function () {
-        $('#documents').DataTable({
-			data: data,
-			columns: [
-				{ data: 'Title' },
-				{ data: 'Category' },
-				{ data: 'Description' },
-				{ data: 'Salesforce ID' },
-				{ data: 'Alternate Tracking ID' },
-				{ data: 'Lead Form Required' },
-				{ data: 'Email to Notify' },
-				{ data: 'Date Added' },
-				{ data: 'Last Modified' }
-			]
-		});
-    });
-
-</script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.15/datatables.min.js"></script>
-
-<?php
-include('includes/footer.php');
-?>
