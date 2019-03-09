32cf33f4fd59b041204aacab84082a8df95fce44
Milder Lisondra <milder.lisondra@yahoo.com>
2019-03-09 10:53:32 -0800

    General backup

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<div>
+        <footer></footer>
+    </div>
+	<script src="../_admin_mm/js/mm.js"></script>
+    <script>
+        $(".logout-btn, #nav_logout").click(function () {
+            window.close();
+        });
+    </script>
+</body>
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,17 @@
+        <div class="navbar">
+            <div>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+					<a href="add.php" class="navbar-brand">Add General Media</a>
+					<a href="add_video.php" class="navbar-brand">Add Video</a>
+                </div>
+
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav"></ul>
+                </div>
+            </div>
+        </div>
